c11e61dbaadf3ecb040fb7962384c43aeaf59e43
DESKTOP-PM8GV6H\Dawid <d.dawid.2000@gmail.com>
2024-06-18 21:42:05 +0200

    update

--- Frontend/src/app/quiz-main-page/quiz-main-page.component.ts
+++ Frontend/src/app/quiz-main-page/quiz-main-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import * as baza from '../../database/baza.json';
 
 @Component({
   selector: 'app-quiz-main-page',
   templateUrl: './quiz-main-page.component.html',
   styleUrl: './quiz-main-page.component.css'
 })
-export class QuizMainPageComponent {
 
+export class QuizMainPageComponent {
+  data: any = baza;
 }
